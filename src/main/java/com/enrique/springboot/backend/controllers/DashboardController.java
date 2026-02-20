4b89bf48d76057ebba5b195b9a7178699805c910
Enrique Israel Torres Mendoza <enrique.torres@maindsoft.net>
2026-02-20 15:09:45 -0600

    eran puros cambios en el front este fue un cambio por error pero ya re ah revertido en este commit y push

--- src/main/java/com/enrique/springboot/backend/controllers/DashboardController.java
+++ src/main/java/com/enrique/springboot/backend/controllers/DashboardController.java
@@ -52,8 +52,7 @@ public class DashboardController {
     //  ResponseEntity<List<RentalDetailDTO>>:
     //  - Retorna HTTp 200 con un JSON array de rentas
     //  - Cada renta incluye: cliente, productos, total, pagado, pendiente.
-    @GetMapping("/" +
-            "deliveries")
+    @GetMapping("/deliveries")
     public ResponseEntity<List<RentalDetailDTO>> getDeliveries(
             @RequestParam(defaultValue = "0") int days) {
         List<RentalDetailDTO> deliveries = dashboardService.getDeliveries(days);
